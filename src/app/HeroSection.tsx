cc3ee5ef1cacc743721a600faeb84a4ae0d46bb3
Aryaman Pandey <a.pandey@dal.ca>
2025-01-05 23:48:17 -0400

    Added Hero Section

--- src/app/HeroSection.tsx
+++ src/app/HeroSection.tsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
+
 export default function HeroSection() {
   return (
-    <div className="flex space-x-6">
-      <div>This will have the text</div>
-      <div>
-        <img src="../public/self-image.jpg" alt="Picture of myself" />
+    <div className="flex items-center justify-center space-x-4 border-2 h-svh text-wrap">
+      <div className="">
+        <div className="flex items-center justify-center p-4 border-2 text-xl">
+          Heyo
+        </div>
+        <div className="border-2 text-lg">
+          I'm Aryaman Pandey, a Bachelor's of CS student at Dalhousie
+          University. Other than Computer Science, I also enjoy working out as
+          well as playing the guitar.
+        </div>
+      </div>
+      <div className="border-2">
+        <Image
+          className=""
+          src="/ownPhoto.jpg"
+          alt="Picture of myself"
+          width={400}
+          height={100}
+        />
       </div>
     </div>
   );
